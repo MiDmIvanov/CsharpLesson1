ba6fcfc0daec8f3889e4ab736b520e03d0338520
admishka <mu@tabletr.ru>
2022-10-03 23:51:22 +0300

    Выполнено задание1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+int max =0;
+if (a>b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
